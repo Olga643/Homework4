f321750eeda12636372d2bfb965a30e2e287e4dc
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:06:59 +0300

    Выяснить является ли число чётным

---
+++ Example4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным
+
+int a = new Random().Next(1,100);
+Console.WriteLine (a);
+
+if (a % 2 == 0) 
+{
+    Console.WriteLine ("Число четное");
+}    
+else
+{
+   Console.WriteLine ("Число не четное"); 
+}
+
+
