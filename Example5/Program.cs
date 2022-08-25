fc00cee23f59dff99ce0ee99821f1d9f92d85c78
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:07:35 +0300

    Показать четные числа от 1 до N

---
+++ Example5/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать четные числа от 1 до N
+
+Console.WriteLine ("Введите чиcло N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = 1; i <= N; i++) 
+
+{
+    if (i % 2 == 0)
+    {
+         Console.WriteLine (i + " "); 
+    }
+}
+Console.WriteLine();
