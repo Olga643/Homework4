abcf429d1dad4eef0ac9bb8de31682f0d0ad602b
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:08:09 +0300

    Показать вторую цифру трёхзначного числа

---
+++ Example6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine ("Введите трехзначное число");
+
+int N = int.Parse(Console.ReadLine() ?? "0");
+int N1 =(N / 10) % 10;
+
+Console. Write (N1);
