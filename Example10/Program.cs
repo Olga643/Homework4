f8fe1eec48c138c6ca0d2eb970a8b07b82e87617
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:10:14 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ Example10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите число");
+
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+ if ( N > 99 )
+{
+  Console.WriteLine(N.ToString()[2]);
+}
+else
+{
+  Console.Write("В этом числе нет третьей цифры");
+}
