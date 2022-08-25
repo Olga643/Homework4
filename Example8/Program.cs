bad9ac42c5ef8d415b054d9e1b75e23d5bbe26d8
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:09:15 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ Example8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine ("Введите трехзначное число");
+
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+if ( N < 100 ^ N > 999) 
+{
+    Console. WriteLine ("Ошибка ввода. Ввведите ТРЕХЗНАЧНОЕ число!!!"); 
+}
+else
+{
+    int N1 = N / 100 * 10 + N % 10;
+    Console.Write(N1); 
+}
+
+
