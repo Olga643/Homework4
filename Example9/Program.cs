9a02d3b3c124317bd61f879edef279ebfa6a3426
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:09:45 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ Example9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int a = new Random().Next(1,100);
+Console.WriteLine (a);
+int b = 6;
+
+if (a % b == 0) 
+{
+    Console.WriteLine ("Выбранное число кратно 6");
+}    
+else
+{
+   Console.WriteLine ( a % b); 
+}
