c194bc05643ce652818efbcdbed1f61cb13bbe97
Olga Rudson <o.rudson@mail.ru>
2022-08-25 20:05:10 +0300

    По заданному номеру дня недели вывести его название

---
+++ Example2/Program.cs
@@ -0,0 +1,12 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine ("Ведите число от 1 до 7");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number == 1) Console.WriteLine ("Monday");
+if (number == 2) Console.WriteLine ("Tuesday");
+if (number == 3) Console.WriteLine ("Wednesday");
+if (number == 4) Console.WriteLine ("Thursday");
+if (number == 5) Console.WriteLine ("HALLELUJAH! FRIDAY!");
+if (number == 6) Console.WriteLine ("Saturday");
+if (number == 7) Console.WriteLine ("Sunday");
